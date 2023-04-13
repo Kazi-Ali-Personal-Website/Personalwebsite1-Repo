1ed0f9d6358a302551e3762a47ed85eb5f41f0ca
Mustafa Ali <mustafa@Mustafa-Mac.local>
2023-04-12 23:13:26 -0500

    Javascript added

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+// To create an initial window alert on every page on the website 
+
+const money = "Hello World,";
+const cash = " Welcome to my Website!";
+
+let code = "The Date And Time Is......";
+
+
+console.log(money + cash);
+window.alert(money + cash);
+
+
+window.alert(code);
+
+code =  Date ();
+
+window.alert(code);
